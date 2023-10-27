d88d55d67da9720c664682fc779f44338579b444
Lev <levan22898@yandex.ru>
2023-10-27 12:34:01 +0300

    pass lab1_1 and lab_1_2

--- lab1_1.py
+++ lab1_1.py
@@ -8,31 +8,42 @@ connection = redis.Redis(host='localhost')
 def decode(settings, key):
     return settings[key].decode()
 
+# Arial, 30, red, italic
+
 def save_settings():
     selected_user = user.get(user.curselection())
-    font = text1.get()
-    font_size = text2.get()
-    font_color = text3.get()
-    font_style = text4.get()
+    # font = text1.get()
+    text_settings = text2.get()
+    # font_color = text3.get()
+    # font_style = text4.get()
     
+    arr = text_settings.split(', ')
+    print(arr)
+
     settings = {
-        'font': font,
-        'font_size': font_size,
-        'font_color': font_color,
-        'font_style': font_style,
+        'font': arr[0],
+        'font_size': arr[1],
+        'font_color': arr[2],
+        'font_style': arr[3],
     }
 
     # хэшируем
     connection.hmset(selected_user, settings)
 
+
 def update_label_text():
+    # Получаем текст надпсии
+    text_title = text5.get()
+  
+
     # Обновляем надпись с учетом текущих настроек
     selected_user = user.get(user.curselection())
     settings = connection.hgetall(selected_user)
+    
     print(settings) #отладочный вывод
 
     # Применяем настройки к тексту в надписи
-    formatted_text = 'This is a sample text.'
+    formatted_text = text_title
     if settings:
         custom_font = (decode(settings, b'font'), decode(settings, b'font_size'), decode(settings, b'font_style'))
         font_color = decode(settings, b'font_color')
@@ -45,22 +56,27 @@ root = tk.Tk()
 root.title('Настройки текстового сообщения')
 root.geometry('600x400')
 
-label2 = tk.Label(root, text='Размер шрифта')
+label2 = tk.Label(root, text='Настройки')
 label2.grid(row=1, column=1)
 text2 = tk.Entry(root)
 text2.grid(row=1, column=2)
-label1 = tk.Label(root, text='Название шрифта')
-label1.grid(row=2, column=1)
-text1 = tk.Entry(root)
-text1.grid(row=2, column=2)
-label3 = tk.Label(root, text='Цвет шрифта')
-label3.grid(row=3, column=1)
-text3 = tk.Entry(root)
-text3.grid(row=3, column=2)
-label4 = tk.Label(root, text='Начертание шрифта')
-label4.grid(row=4, column=1)
-text4 = tk.Entry(root)
-text4.grid(row=4, column=2)
+# label1 = tk.Label(root, text='Название шрифта')
+# label1.grid(row=2, column=1)
+# text1 = tk.Entry(root)
+# text1.grid(row=2, column=2)
+# label3 = tk.Label(root, text='Цвет шрифта')
+# label3.grid(row=3, column=1)
+# text3 = tk.Entry(root)
+# text3.grid(row=3, column=2)
+# label4 = tk.Label(root, text='Начертание шрифта')
+# label4.grid(row=4, column=1)
+# text4 = tk.Entry(root)
+# text4.grid(row=4, column=2)
+
+label5 = tk.Label(root, text='Текст надписи')
+label5.grid(row=5, column=1)
+text5 = tk.Entry(root)
+text5.grid(row=5, column=2)
 
 user = tk.Listbox(root, width=30, height=3)
 user.insert(0, 'Пользователь №1')
@@ -76,5 +92,7 @@ label = tk.Label(root, text='This is a sample text.')
 label.grid(row=6, column=1, columnspan=3)
 
 user.bind('<<ListboxSelect>>', lambda event: update_label_text())
+text5.bind('<KeyRelease>', lambda event: update_label_text())  # Привязываем событие KeyRelease к полю ввода текста
+
 
 root.mainloop()
--- lab2_2.py
+++ lab2_2.py
@@ -11,8 +11,16 @@ def add_score():
     judge = judge_var.get()
     score = int(score_entry.get())
 
+    if (score < 0): 
+        return    
+    
+
     # Создаем ключ в виде "sportsperson:judge" и сохраняем оценку
     key = f"sportsperson:{sportsperson}:{judge}"
+
+    if (check_judges(key) == True):
+        return
+
     connection.hincrby(key, "score", score)
     update_rankings()
 
@@ -25,7 +33,7 @@ def update_rankings():
 
 # Функция для получения рейтинга
 def get_rankings():
-    # Получите всех спортсменов из базы данных
+    # Получем из бд спортсменов всех
     sportspersons = set()
     for key in connection.keys('sportsperson:*'):
         sportsperson = key.decode().split(':')[1]
@@ -41,18 +49,36 @@ def get_rankings():
     sorted_rankings = sorted(rankings.items(), key=lambda x: x[1], reverse=True)
     return sorted_rankings
 
+
+# Проверка ставил ли судья этому спортсмену баллы
+def check_judges(key):
+    for k in connection.keys('sportsperson:*'):
+        print(k)
+            
+    return connection.exists(key)
+
+
+def clear_rankings():
+    for sportsperson in sportsmens:
+        for judge in judges:
+            key = f"sportsperson:{sportsperson}:{judge}"
+            connection.delete(key)
+
 # Создание окна
 root = tk.Tk()
 root.title("Спортивный монитор")
 root.geometry('300x400')
 
+sportsmens = ["Спортсмен 1", "Спортсмен 2", "Спортсмен 3", "Спортсмен 4", "Спортсмен 5"]
+judges = ["Судья 1", "Судья 2", "Судья 3", "Судья 4", "Судья 5"]
+
 # Создание и настройка виджетов
 sportsperson_label = tk.Label(root, text="Спортсмен:")
 sportsperson_var = tk.StringVar()
-sportsperson_combobox = ttk.Combobox(root, textvariable=sportsperson_var, values=["Спортсмен 1", "Спортсмен 2"])
+sportsperson_combobox = ttk.Combobox(root, textvariable=sportsperson_var, values=sportsmens)
 judge_label = tk.Label(root, text="Судья:")
 judge_var = tk.StringVar()
-judge_combobox = ttk.Combobox(root, textvariable=judge_var, values=["Судья 1", "Судья 2"])
+judge_combobox = ttk.Combobox(root, textvariable=judge_var, values=judges)
 score_label = tk.Label(root, text="Баллы:")
 score_entry = tk.Entry(root)
 save_button = tk.Button(root, text="Сохранить баллы", command=add_score)
@@ -69,6 +95,8 @@ score_entry.grid(row=2, column=1)
 save_button.grid(row=3, column=0, columnspan=2)
 rankings_label.grid(row=4, column=0, columnspan=2)
 rankings_listbox.grid(row=5, column=0, columnspan=2)
+
+clear_rankings()
 
 # Запуск приложения
 root.mainloop()
